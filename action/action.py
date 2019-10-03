7eebc826777ba2fb0222cb88f3501eef1b16a121
wltiii <bill.turner@changent.com>
2019-10-03 10:53:34 -0500

    stubbed in sessions

--- action/action.py
+++ action/action.py
@@ -9,11 +9,14 @@ time_unit = 'minutes'
 
 goal = input(f'What do you plan to do in the next {time_allotment} {time_unit}? ')
 starttime = datetime.now(timezone.utc)
+session = {}
+session['name'] = 'sample'
+session['duration_in_millis'] = 1000
+planned_sessions = {}
 
 experience = input(f'What happened during the allotted time? ')
 difference = input(f'What explains the difference? ')
 action = input(f'What action can you take to improve outcomes? ')
-
 
 report = {
    'start': f'{starttime}',
@@ -28,3 +31,4 @@ y = json.dumps(report)
 
 # the result is a JSON string:
 print(y)
+print(json.dumps(session))
