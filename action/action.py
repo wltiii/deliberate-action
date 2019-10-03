b45bad89a12b6516510fcc1e9e82835423a6e3bf
wltiii <bill.turner@changent.com>
2019-10-03 07:44:15 -0500

    added start time to report

--- action/action.py
+++ action/action.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime, timezone
 
 print('Deliberate Action - Plan, Do, Reflect - and Learn')
 print('.... Refine your mental models ....')
@@ -6,13 +7,17 @@ print('.... Refine your mental models ....')
 time_allotment = 25
 time_unit = 'minutes'
 
-intention = input(f'What do you plan to do in the next {time_allotment} {time_unit}? ')
+goal = input(f'What do you plan to do in the next {time_allotment} {time_unit}? ')
+starttime = datetime.now(timezone.utc)
+
 experience = input(f'What happened during the allotted time? ')
 difference = input(f'What explains the difference? ')
 action = input(f'What action can you take to improve outcomes? ')
 
+
 report = {
-   'intention' : f'{intention}',
+   'start': f'{starttime}',
+   'goal' : f'{goal}',
    'experience' : f'{experience}',
    'difference' : f'{difference}',
    'action' : f'{action}'
