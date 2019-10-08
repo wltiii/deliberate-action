df4b49db1606e78b951fa6dd2b42fc05ba54c645
wltiii <github.0vx@gishpuppy.com>
2019-10-08 13:21:11 -0500

    updated README

--- action/simpleaudio_alarm.py
+++ action/simpleaudio_alarm.py
@@ -1,7 +1,8 @@
 # pip3 install simpleaudio
 import simpleaudio as sa
 
-# music files
+# music files - how to convert files to buffered. audio_data parameter must be an
+# object which supports the buffer interface. (bytes objects, Python arrays, and Numpy arrays all qualify.)
 # f = '/home/worldwidewilly/workspace/deliberate-action/action/on-the-run-snippet.mp3'
 f = '/home/worldwidewilly/workspace/deliberate-action/action/on-the-run-snippet.wav'
 # f = '/home/worldwidewilly/workspace/deliberate-action/action/echoes_ping.wav'
@@ -14,16 +15,11 @@ def ring():
     # sound_alarm = True
     while True:
         try:
-            # print(f'sound_alarm is {sound_alarm}')
-            # if sound_alarm == False:
-            #     return
             play_obj = wave_obj.play()
             play_obj.wait_done()
         except KeyboardInterrupt:
-            # sound_alarm = False
             play_obj.stop()
             print('')
-            # print('done playing sound.')
             break
 
 if __name__ == "__main__":
