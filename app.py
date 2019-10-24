18e66f84389091af5e9247f1380084a28fd35eeb
wltiii <github.0vx@gishpuppy.com>
2019-10-24 10:01:02 -0500

    added REST methods for sessions and templates

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask import Response
 from flask_pymongo import PyMongo
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from flask import jsonify, request
+import logging
+logging.basicConfig(filename='app.log',level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/deliberate-action'
 mongo = PyMongo(app)
-from bson.json_util import dumps
-from bson.objectid import ObjectId
-from flask import jsonify, request
 
 @app.route('/')
 def index():
@@ -14,31 +17,64 @@ def index():
 
 @app.route('/templates')
 def getTemplates():
+    logging.debug('getTemplates()')
     templates = mongo.db.templates.find({})
     resp = dumps(templates)
     return resp
 
-@app.route('/template/<type>/<name>/<user>')
-def getTemplate(type, name, user):
-    print(f'getTemplate({type}, {name}, {user})')
+@app.route('/templates/<id>')
+def findById(id):
+    logging.debug(f'findById({id})')
+    query = {
+        "_id": ObjectId(id)
+    }
+    template = mongo.db.templates.find_one_or_404(query)
+    resp = dumps(template)
+    return Response(resp, mimetype='application/json')
+
+@app.route('/templates/<type>/<name>/<user>')
+def findByKey(type, name, user):
+    logging.debug(f'findByKey({type}, {name}, {user})')
     query = {
         "type": f'{type}',
         "name": f'{name}',
         "user": int(user)
     }
-    print (f'query is {query}')
-    default_template = mongo.db.templates.find(query)
-    resp = dumps(default_template)
-    return resp
+    template = mongo.db.templates.find_one_or_404(query)
+    resp = dumps(template)
+    return Response(resp, mimetype='application/json')
+
+@app.route('/templates/default')
+def getDefaultTemplate():
+    logging.debug('getDefaultTemplate()')
+    return findByKey('session', 'default', 0)
 
 @app.route('/sessions')
 def getSessions():
-    default_template = mongo.db.templates.find({
-        "type": "session",
-        "name": "default",
-        "user": 0
-    })
-    resp = dumps(default_template)
+    logging.debug('getSessions()')
+    sessions = mongo.db.sessions.find()
+    resp = dumps(sessions)
+    return resp
+
+@app.route('/sessions', methods=['POST'])
+def addSession():
+    logging.debug('addSession()')
+    sessions = mongo.db.sessions.find()
+    resp = dumps(sessions)
+    return resp
+
+@app.route('/sessions/<id>', methods=['PUT'])
+def updateSession(id):
+    logging.debug('updateSession()')
+    sessions = mongo.db.sessions.find()
+    resp = dumps(sessions)
+    return resp
+
+@app.route('/sessions/<id>', methods=['DELETE'])
+def deleteSession(id):
+    logging.debug('deleteSession()')
+    sessions = mongo.db.sessions.find()
+    resp = dumps(sessions)
     return resp
 
 if __name__ == '__main__':
